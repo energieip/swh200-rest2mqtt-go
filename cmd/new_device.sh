3fd1fed000eeaab7bbdc2680f694f262336abb90
Amandine Drebes <amandine@energie-ip.com>
2019-10-29 14:18:42 +0100

    Remove old log

--- cmd/new_device.sh
+++ cmd/new_device.sh
@@ -19,10 +19,9 @@ oid=${mac:0:8}
 oid=$(echo ${oid^^})
 
 if [ "$oid" == "F2:23:D0" ]; then
-  echo "$(date) EIP device skip it $mac" >> /tmp/log.txt
   exit 0
 fi
 
-echo "$(date) Get $1 IP: $ip MAC: $mac" >> /tmp/log.txt
+#echo "$(date) Get $1 IP: $ip MAC: $mac" >> /tmp/log.txt
 
 $(new-device -c /etc/energieip-swh200-rest2mqtt/config.json -i "$ip" -m "$mac")
